ea28f99c02b0232d73051f1d0fcb1e09c4b896d3
liboran <liboran@baidu.com>
2024-06-28 17:22:59 +0800

    record

--- coder/pretrained/load_model.py
+++ coder/pretrained/load_model.py
@@ -5,6 +5,9 @@ import sys
 sys.path.append('/ssd1/liboran/gitspace/github_Librarvl/LLM-experiment/')
 
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
+from transformers import Trainer, TrainingArguments
+
+
 import pandas as pd
 from datasets import load_dataset
 import torch.utils.data.dataloader as DataLoader
@@ -23,12 +26,29 @@ def tf_load_dataset():
 
 
 def load_model():
-    data_set = tf_load_dataset()
+    # data_set = tf_load_dataset()
+    path = "/home/work/wenku_yq/DataVault/models/Meta-Llama-3-8B"
+    path = "/home/work/wenku_yq/DataVault/models/Llama2-7b-hf"
+    llama_config = AutoConfig.from_pretrained(path)
+    # llama_config = LlamaConfig()
+    llama_model = LlamaModel(path)
 
-    llama_config = LlamaConfig()
-    llama_model = LlamaModel(llama_config)
+    training_args = TrainingArguments(
+        output_dir='./results',
+        num_train_epochs=1,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=64,
+        warmup_steps=500,
+        weight_decay=0.01,
+        logging_dir='./logs',
+    )
 
-    a = 1
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+    )
 
     # llama_model
 
